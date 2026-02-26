d5420e3d78a224bf06090dad5c443b6b7aa0d49d
Dima Limanets <limanets@ualberta.ca>
2026-02-25 17:20:32 -0700

    Completed

--- app/src/main/java/com/example/androiduitesting/MainActivity.java
+++ app/src/main/java/com/example/androiduitesting/MainActivity.java
@@ -41,6 +41,13 @@ public class MainActivity extends AppCompatActivity {
 
         cityList.setAdapter(cityAdapter);
 
+        cityList.setOnItemClickListener((parent, view, position, id) ->{
+            String cityName = dataList.get(position);
+            android.content.Intent intent = new android.content.Intent(MainActivity.this, ShowActivity.class);
+            intent.putExtra("city_name", cityName);
+            startActivity(intent);
+        });
+
         final Button addButton = findViewById(R.id.button_add);
         addButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
@@ -64,5 +71,7 @@ public class MainActivity extends AppCompatActivity {
                 cityAdapter.clear();
             }
         });
+
+
     }
 }
